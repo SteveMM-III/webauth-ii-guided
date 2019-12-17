48a0d249753e6ccf281c134264fda6abc228bcfd
Kronykus <MoreheadSteve@gmail.com>
2019-12-17 12:38:04 -0600

    auth

--- api/server.js
+++ api/server.js
@@ -1,21 +1,50 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express  = require( 'express'         );
+const helmet   = require( 'helmet'          );
+const cors     = require( 'cors'            );
+const sessions = require( 'express-session' );
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const KnexSessionStore = require( 'connect-session-knex' )( sessions ); // store session in DB
+
+const authRouter = require ( '../auth/auth-router.js'   );
+const usersRouter = require( '../users/users-router.js' );
+
+const knex = require( '../database/dbConfig' );
 
 const server = express();
 
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
+const tenMin = 1000 * 60 * 10;
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+const sessionConfiguration = {
+  name: 'cookieCrisp',
+  secret: 'keep it secret, keep it safe!',  // for encryption
+  saveUninitialized: true,  // GDPR laws require false
+  resave: false,
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
-});
+  store: new KnexSessionStore( {
+    knex,
+    createtable: true,
+    clearInterval: tenMin,
+    sidfieldname: 'sid',
+    tablename: 'sessions'
+  } ),
+
+  cookie: {
+    maxAge: tenMin,
+    secure: false,   // false for http: development; true for https: production
+    httpOnly: true,  // if true JS can't touch the cookie
+  }
+};
+
+server.use( helmet()                         );
+server.use( express.json()                   );
+server.use( cors()                           );
+server.use( sessions( sessionConfiguration ) );  // add req.session object
+
+server.use( '/api/auth', authRouter   );
+server.use( '/api/users', usersRouter );
+
+server.get( '/', ( req, res ) => {
+  res.json( { api: 'up' }   );
+} );
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,8 +5,9 @@ const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
-  let user = req.body;
+  let user   = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  
   user.password = hash;
 
   Users.add(user)
@@ -25,6 +26,9 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+
+        req.session.user = user;
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
